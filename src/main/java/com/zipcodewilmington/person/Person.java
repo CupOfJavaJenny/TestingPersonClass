bebbe7c9a1a9779de1fcf52532f9b6a6e92d13c1
Jennifer <jennifer@Zipcoders-MacBook-Pro.local>
2024-03-08 15:02:38 -0500

    finished class, not test, still working on it

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,96 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String eyeColor;
+    private boolean canSee;
+    private char middleInit;
+    private String lName;
+    private boolean jr;
 
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
+    public Person(String name, int age, String eyeColor, boolean canSee, char middleInit, String lName, boolean jr) {
+        this.name = name;
+        this.age = age;
+        this.eyeColor = eyeColor;
+        this.canSee = canSee;
+        this.middleInit = middleInit;
+        this.lName = lName;
+        this.jr = jr;
+    }
+
+
     public void setName(String name) {
+
+    this.name = name;
+    }
+
+    public String getName(){
+        return name;
     }
 
     public void setAge(int age) {
+    this.age = age;}
+
+    public int getAge() {
+
+        return age;
     }
 
-    public String getName() {
-        return null;
+    public String getEyeColor() {
+        return eyeColor;
+    }
+    public void setEyeColor(String eyeColor){
+        this.eyeColor = eyeColor;
     }
 
-    public Integer getAge() {
-        return null;
+    public boolean getCanSee(){
+        return canSee;
     }
+
+    public void setCanSee(boolean canSee){
+        this.canSee = canSee;
+    }
+
+    public char getMiddleInit(){
+        return middleInit;
+    }
+
+    public void setMiddleInit(char middleInit){
+        this.middleInit = middleInit;
+    }
+
+    public String getLName(){
+        return lName;
+    }
+
+    public void setLName(String lName){
+     this.lName = lName;
+    }
+
+    public boolean getJR(){
+        return jr;
+    }
+
+    public void setJR(boolean jr){
+        this.jr = jr;
+    }
+
 }
